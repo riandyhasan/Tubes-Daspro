61fba2bfb503d000a58aed7bbf5f9aa012df4d0f
Riandy Hasan <16520117@mahasiswa.itb.ac.id>
2021-04-13 21:50:51 +0800

    update F03

--- CariRarity.py
+++ CariRarity.py
@@ -1,4 +1,4 @@
-f = open("user.csv","r")
+f = open("gadget.csv","r")
 raw_lines = f.readlines()
 f.close()
 lines = [raw_line.replace('"', "") for raw_line in raw_lines]
@@ -6,7 +6,7 @@ lines = [raw_line.replace('"', "") for raw_line in raw_lines]
 def convert_array_data_to_real_values(array_data):
   arr_cpy = array_data[:]
   for i in range(6):
-    if(i == 0):
+    if(i == 3) or (i == 5):
       arr_cpy[i] = int(arr_cpy[i])
   return arr_cpy
 
@@ -25,17 +25,37 @@ def convert_string_to_array(line):
           word = ""
   return new_list
 
-def validate_account(log_username, log_password, data, n):
-  idx = -1
-  for i in range (n):
-    if log_username == data[i][1] and log_password  == data[i][4]:
-      idx = i
-  return (idx)
+def rarity_check(find, gadget_rarity):
+    check = False
+    if (find == gadget_rarity):
+        check = True
+    return check
+
 
 raw_header = lines.pop(0)
 header = convert_string_to_array(raw_header)
-data_akun = []
+data_gadget = []
 for line in lines:
   array_of_data = convert_string_to_array(line)
   real_values = convert_array_data_to_real_values(array_of_data)
-  data_akun.append(real_values)
+  data_gadget.append(real_values)
+
+print("======================================")
+print("============ CARI RARITY =============")
+print("======================================\n")
+
+rare = input("Masukkan rarity: ")
+
+print("\nHasil pencarian:")
+count = 0
+for i in range (len(lines)):
+    if rarity_check(rare, data_gadget[i][4]):
+        print("\nNama             : {}".format(data_gadget[i][1]))
+        print("Deskripsi        : {}".format(data_gadget[i][2]))
+        print("Jumlah           : {}".format(data_gadget[i][3]))
+        print("Rarity           : {}".format(data_gadget[i][4]))
+        print("Tahun Ditemukan  : {}".format(data_gadget[i][5]))
+        count += 1
+
+if count == 0:
+    print("Tidak ada gadget yang ditemukan")
