d05e9ac15e3152793bce2f9a9fb57533e2896c3d
Fildzahishmah <73594291+Fildzahishmah@users.noreply.github.com>
2021-04-30 14:23:39 +0700

    Update pinjam.py

--- pinjam.py
+++ pinjam.py
@@ -69,11 +69,12 @@ def pinjam():
         if (cek_id):
             print("Tidak ada item dengan ID tersebut.")
         else:
+            username = input("Masukkan Username: ")
             tanggal_peminjaman = input("Tanggal peminjaman: ")
             jumlah_peminjaman = int(input("Masukkan Jumlah: "))
             jumlah_akhir_gadget = datas_gadget[i][3] - jumlah_peminjaman
             datas_gadget[i][3] = jumlah_akhir_gadget
-            new_history = [add_id_history, username_login, id, tanggal_peminjaman, jumlah_peminjaman, is_returned]
+            new_history = [add_id_history, username, id, tanggal_peminjaman, jumlah_peminjaman, is_returned]
             history_data_gadget.append(new_history)
             print("Item" + datas_gadget[i][1] + "(x" + str(jumlah_peminjaman) + ") berhasil dipinjam!")
     else:
